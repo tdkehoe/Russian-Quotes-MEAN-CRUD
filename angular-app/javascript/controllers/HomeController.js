da8d34f47d8b1cf9e8d20720012e7b52712adb06
Thomas David Kehoe <kehoe@casafuturatech.com>
2015-09-11 11:33:34 -0600

    Delete comments working.

--- angular-app/javascript/controllers/HomeController.js
+++ angular-app/javascript/controllers/HomeController.js
@@ -16,7 +16,33 @@ app.controller('HomeController', ["$scope", '$http', '$location', '$route', '$ro
     });
   }
 
-  // Comments function
+  $scope.upLike = function(quote) {
+    console.log("Liked!");
+    console.log(quote);
+    var likes = quote.likes || 0;
+    quote.likes += 1;
+    console.log(likes);
+    $http.put('http://localhost:8080/api/quotes/' + quote._id, quote).then(function(response) { // UPDATE
+      console.log("Upliked.");
+    }, function(response) {
+      console.log("Invalid URL");
+    });
+  }
+
+  $scope.downLike = function(quote) {
+    console.log("Not liked!");
+    console.log(quote);
+    var likes = quote.likes || 0;
+    quote.likes -= 1;
+    console.log(likes);
+    $http.put('http://localhost:8080/api/quotes/' + quote._id, quote).then(function(response) { // UPDATE
+      console.log("Downliked.");
+    }, function(response) {
+      console.log("Invalid URL");
+    });
+  }
+
+
   $scope.newComment = function(quote) {
     var comment = {
       commentAuthor: quote.newComment.commentAuthor,
@@ -35,6 +61,18 @@ app.controller('HomeController', ["$scope", '$http', '$location', '$route', '$ro
       console.log("Invalid URL");
     });
   }; // closes newComment
+
+  $scope.deleteComment = function(quote, comment) {
+    var index = quote.comments.indexOf(comment);
+    quote.comments.splice(index, 1);
+    $http.put('http://localhost:8080/api/quotes/' + quote._id, quote).then(function(response) { // UPDATE
+      $location.path( "/" );
+      console.log("Comment deleted.");
+    }, function(response) {
+      console.log("Invalid URL");
+    });
+  } // closes deleteComment
+
 }]);
 
 app.controller('NewController', ["$scope", '$http', '$location', '$route', function($scope, $http, $location, $route){
